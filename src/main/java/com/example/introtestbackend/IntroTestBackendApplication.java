fb047f376c1227aae5304afc720ae70e88b787c4
Lukas <larurau@gmail.com>
2024-03-05 13:14:53 +0100

    Initial commit

---
+++ src/main/java/com/example/introtestbackend/IntroTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.introtestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroTestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroTestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/introtestbackend/IntroTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.introtestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroTestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
